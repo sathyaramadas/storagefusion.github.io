79480539c986b9c40bddcb193719bf396451fe22
sathyaramadas <52823172+sathyaramadas@users.noreply.github.com>
2024-01-18 10:36:45 +0530

    Update slides.js

--- slides-animated-landing-page-master/js/slides.js
+++ slides-animated-landing-page-master/js/slides.js
@@ -1099,6 +1099,7 @@ $(document).ready(function() { "use strict";
 
           // Add Tooltips
           var title = $('.slide:not(.exclude):eq('+(i - 1)+')').data('title');
+           title = 'test'
           if (title === undefined) {
             $(element).append('<li data-title="title'"></li>');
           } else {
